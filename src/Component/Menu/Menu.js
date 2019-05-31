086983a59a701b3ec132faf5e83a61b360510478
borisgrunwald <borisgrunwald@gmail.com>
2019-05-31 12:48:59 +0200

    Changed menu text

--- src/Component/Menu/Menu.js
+++ src/Component/Menu/Menu.js
@@ -5,7 +5,7 @@ import Button from "../../UI/Button/Button";
 const Menu = props => {
     return (
         <div className="menu">
-            <h2>Choose Difficulty</h2>
+            <h2>Choose The Difficulty</h2>
             <Button clicked={() => props.diffSelected('easy')}>Easy</Button>
             <Button clicked={() => props.diffSelected('medium')} >Medium</Button>
             <Button clicked={() => props.diffSelected('hard')} >Hard</Button>
